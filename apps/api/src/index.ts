0526447f383de6f3407c0e408fc554243825018b
MohdFazil45 <mdfazil846@gmail.com>
2026-07-28 15:41:01 +0530

    fixed #5

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,24 +1,25 @@
-import express from "express"
-const app: express.Express = express()
-import userRouter from "./user/routes"
-import contestRouter from "./contest/routes"
-import challengeRouter from "./challenges/routes"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+const app: express.Express = express();
+import userRouter from "./user/routes";
+import contestRouter from "./contest/routes";
+import challengeRouter from "./challenges/routes";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-app.use(cors({
-    origin:"http://localhost:3000",
-    methods:["GET","POST","PUT","DELETE","OPTIONS","PATCH"],
-    allowedHeaders:["Content-Type","Authorization"],
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://fazil-devwarrior.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
-app.use(cookieParser())
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/contest",contestRouter)
-app.use("/api/v1/challenge",challengeRouter)
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/contest", contestRouter);
+app.use("/api/v1/challenge", challengeRouter);
 
-
-export default app
+export default app;
