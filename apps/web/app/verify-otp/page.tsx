7486da57730c3d7e03f84308616bef6cb0df4595
MohdFazil45 <mdfazil846@gmail.com>
2026-07-28 14:46:23 +0530

    fixed #2

--- apps/web/app/verify-otp/page.tsx
+++ apps/web/app/verify-otp/page.tsx
@@ -5,7 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { env } from "../../config/env";
 import { toast } from "sonner";
 import OtpInput from "../../components/OTPInput";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function OTPVerificationCard() {
   const searchParams = useSearchParams();
@@ -79,42 +79,44 @@ export default function OTPVerificationCard() {
 
   console.log(email);
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/70 backdrop-blur-sm px-4">
-      <div className="w-full max-w-md rounded-md border border-neutral-700 bg-neutral-900 p-8 shadow-2xl">
-        <div className="flex flex-col items-center">
-          <div className="mb-5 flex h-16 w-16 items-center justify-center rounded-full bg-orange-500/15">
-            <ShieldCheck className="h-8 w-8 text-orange-400" />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="fixed inset-0 flex items-center justify-center bg-black/70 backdrop-blur-sm px-4">
+        <div className="w-full max-w-md rounded-md border border-neutral-700 bg-neutral-900 p-8 shadow-2xl">
+          <div className="flex flex-col items-center">
+            <div className="mb-5 flex h-16 w-16 items-center justify-center rounded-full bg-orange-500/15">
+              <ShieldCheck className="h-8 w-8 text-orange-400" />
+            </div>
+
+            <h1 className="text-2xl font-bold text-white">Verify your email</h1>
+
+            <p className="mt-2 text-center text-sm text-neutral-400">
+              We've sent a 6-digit verification code to
+            </p>
+
+            <p className="mt-1 font-medium text-orange-400">{email}</p>
           </div>
 
-          <h1 className="text-2xl font-bold text-white">Verify your email</h1>
+          {/* OTP Inputs */}
+          <div className="mt-8 flex justify-center gap-3">
+            <OtpInput onComplete={(otp) => handleOtpVerification(otp)} />
+          </div>
 
-          <p className="mt-2 text-center text-sm text-neutral-400">
-            We've sent a 6-digit verification code to
-          </p>
-
-          <p className="mt-1 font-medium text-orange-400">{email}</p>
-        </div>
-
-        {/* OTP Inputs */}
-        <div className="mt-8 flex justify-center gap-3">
-          <OtpInput onComplete={(otp) => handleOtpVerification(otp)} />
-        </div>
-
-        <button className="mt-8 w-full rounded-xl bg-linear-to-r from-orange-500 to-orange-400 py-3 font-semibold text-white transition hover:scale-[1.02] hover:shadow-lg hover:shadow-orange-500/20">
-          Verify OTP
-        </button>
-
-        <div className="mt-6 text-center">
-          <p className="text-sm text-neutral-400">Didn't receive the code?</p>
-
-          <button
-            onClick={resendOtp}
-            className={`mt-2 text-sm font-medium  transition ${isButtonDisabled ? "cursor-none text-red-400" : "cursor-pointer text-orange-400"}`}
-          >
-            Resend OTP <span>{resendTimeLeft}s</span>
+          <button className="mt-8 w-full rounded-xl bg-linear-to-r from-orange-500 to-orange-400 py-3 font-semibold text-white transition hover:scale-[1.02] hover:shadow-lg hover:shadow-orange-500/20">
+            Verify OTP
           </button>
+
+          <div className="mt-6 text-center">
+            <p className="text-sm text-neutral-400">Didn't receive the code?</p>
+
+            <button
+              onClick={resendOtp}
+              className={`mt-2 text-sm font-medium  transition ${isButtonDisabled ? "cursor-none text-red-400" : "cursor-pointer text-orange-400"}`}
+            >
+              Resend OTP <span>{resendTimeLeft}s</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
